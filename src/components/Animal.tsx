8f5bbd0186ec5eede6503bd901132efdad12e36e
jenmwa <jenmwa@gmail.com>
2023-08-17 13:51:03 +0200

    useParams to find animal:Id

--- src/components/Animal.tsx
+++ src/components/Animal.tsx
@@ -1,10 +1,12 @@
+import { useParams } from "react-router-dom"
 
 
 export const Animal = () => {
 
+  const {id} = useParams();
  
 
     return <>
-      <p>AnimalInfo</p>
+      <p>AnimalInfo {id}</p>
     </>
   }
