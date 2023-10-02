289e015871026091fbaf9caec6243365aa4e2e70
jenmwa <jenmwa@gmail.com>
2023-10-02 19:39:06 +0200

    logo import upd.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,21 +1,24 @@
-import { Link } from 'react-router-dom'
-import '../style/home.scss'
+import { Link } from "react-router-dom";
+import "../style/home.scss";
+import logo from "/logo_light.png";
 
 export const Home = () => {
-
-  return <>
-   
-    <div className='hero-img'>
-      <div className='hero-info'>
-      <p>Välkomna till</p>
-        <div className='logo-container'>
-          <img src='/logo_light.png' alt='the zoo logo' width={375} />
+  return (
+    <>
+      <div className="hero-img">
+        <div className="hero-info">
+          <p>Välkomna till</p>
+          <div className="logo-container">
+            <img src={logo} alt="the zoo logo" width={375} />
+          </div>
+          <p>
+            AKA<br></br>Djuren behöver mat!
+          </p>
+          <Link to="/animals">
+            <button>Våra djur</button>
+          </Link>
         </div>
-        <p>AKA<br></br>Djuren behöver mat!</p>
-        <Link to='/animals'>
-          <button>Våra djur</button>
-        </Link>
       </div>
-    </div>
-  </>
-}
+    </>
+  );
+};
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,22 +1,32 @@
 import { Link } from "react-router-dom";
-import '../style/navigation.scss'
+import "../style/navigation.scss";
+import logo from "/PNG/logo_light.png";
 
 export const Navigation = () => {
-  return <>
-    <nav className='navigation'>
-      <div className='logo-container'>
-          <Link to='/'>
-        <img src='/PNG/logo_light.png' alt='the zoo logo' width={80} />
-        </Link>
-      </div>
-      <div className='menu'>
-        <ul>
-          <li><Link to='/'>Hem</Link></li>
-          <li><Link to='/about'>Om</Link></li>
-          <li><Link to='/animals'><div>Våra djur</div></Link></li>
-        </ul>
-      </div>
-
-    </nav>
-  </>
-}
+  return (
+    <>
+      <nav className="navigation">
+        <div className="logo-container">
+          <Link to="/">
+            <img src={logo} alt="the zoo logo" width={80} />
+          </Link>
+        </div>
+        <div className="menu">
+          <ul>
+            <li>
+              <Link to="/">Hem</Link>
+            </li>
+            <li>
+              <Link to="/about">Om</Link>
+            </li>
+            <li>
+              <Link to="/animals">
+                <div>Våra djur</div>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
